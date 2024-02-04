584dbb09bcced1e6a5165caffaa170248be01bd9
ChoiEunCheol <wndqhrgkdlel@gmail.com>
2024-02-04 22:36:48 +0900

    crop - 이미지 영역 설정

--- ocr_test.py
+++ ocr_test.py
@@ -5,9 +5,12 @@ import pytesseract
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
 # 이미지 파일 불러오기
-image = Image.open('./example_image1.png')
+image = Image.open('./test_image1.jpg')
+
+# 텍스트가 있는 영역만 크롭하기
+cropped_image = image.crop((210, 150, 660, 560))
 
 # 이미지에서 텍스트 추출
-text = pytesseract.image_to_string(image, lang='eng+kor')
+text = pytesseract.image_to_string(cropped_image, lang='eng+kor')
 
 print(text)
