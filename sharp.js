1f0f015c4c02fec87edbe204b4e76cc13cc6f058
최은철 <wndqhrgkdlel@gmail.com>
2024-02-08 15:52:34 +0900

    이미지 전처리 과정

---
+++ sharp.js
@@ -0,0 +1,14 @@
+const sharp = require('sharp');
+
+sharp('./test_image1.jpg')
+//   .resize(800) // 너비를 800px로 조정
+  .greyscale() // 흑백 이미지로 변환
+  .modulate({
+    brightness: 1.2,
+    saturation: 1.5,
+  })
+  .linear(2, -190) 
+  .toFile('sharp-image1.jpg', (err, info) => { 
+    if (err) throw err;
+    console.log(info);
+  });
