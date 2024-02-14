b38aabf52c1ef5d4ebf0043b95853d02f7f91be1
최은철 <wndqhrgkdlel@gmail.com>
2024-02-14 16:10:49 +0900

    이미지 자르기

--- sharp.js
+++ sharp.js
@@ -1,14 +1,16 @@
 const sharp = require('sharp');
 
-sharp('./test_image1.jpg')
-//   .resize(800) // 너비를 800px로 조정
-  .greyscale() // 흑백 이미지로 변환
-  .modulate({
-    brightness: 1.2,
-    saturation: 1.5,
+// 이미지 파일 경로
+const inputImagePath = './test_image1.jpg';
+const outputImagePath = './cutting1.jpg';
+
+// 이미지 자르기
+sharp(inputImagePath)
+  .extract({ left: 530, top: 150, width: 55, height: 415 }) // 자를 영역 지정
+  .toFile(outputImagePath)
+  .then(() => {
+    console.log('이미지가 성공적으로 자르기 되었습니다.');
   })
-  .linear(2, -190) 
-  .toFile('sharp_image1.jpg', (err, info) => { 
-    if (err) throw err;
-    console.log(info);
+  .catch(err => {
+    console.error('이미지 자르기 오류:', err);
   });
--- tesseract.js
+++ tesseract.js
@@ -1,14 +1,13 @@
 const Tesseract = require('tesseract.js');
 const path = require('path');
 
-const imagePath = path.join(__dirname, './test_image1.jpg'); // 이미지 파일 경로
+const imagePath = path.join(__dirname, './cutting1.jpg'); // 이미지 파일 경로
 
 Tesseract.recognize(
     imagePath,
     'kor', // 또는 다른 언어 코드
     {
         logger: m => console.log(m), // 진행 상황 로그
-        rectangle: { left: 520, top: 150, width: 70, height: 450 },// 인식할 영역 지정
         tessedit_char_whitelist: '0123456789', // 숫자만 인식하도록 설정
     }
 ).then(({ data: { text } }) => {
